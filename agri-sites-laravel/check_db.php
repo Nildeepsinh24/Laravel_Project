5272633013a37be4e710adc1886495dfe41e5cd5
Neeldeepsinh Jadeja <neeldeepsinhjadeja24@gmail.com>
2026-01-28 17:47:17 +0530

    Removed unused files for better understanding

--- agri-sites-laravel/check_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db = new PDO('sqlite:database/database.sqlite');
-$result = $db->query("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name");
-echo "Tables in database:\n";
-while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    echo "- " . $row['name'] . "\n";
-}
-?>
