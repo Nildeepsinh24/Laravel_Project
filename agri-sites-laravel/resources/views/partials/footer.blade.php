c4dcfe04e3a3b8375b001bd086d94b7763eeaca6
Neeldeepsinh Jadeja <neeldeepsinhjadeja24@gmail.com>
2026-02-04 00:12:18 +0530

    Redesign footer - three column layout and remove utility pages

--- agri-sites-laravel/resources/views/partials/footer.blade.php
+++ agri-sites-laravel/resources/views/partials/footer.blade.php
@@ -1,74 +1,54 @@
 <div class="main-footer-section-ed">
     <div class="container">
-        <div class="row footer-main-row-mn">
-        <div class="col-md-3 footer-one-clm">
-            <ul class="ftr-mn-ul">
-                <h4 data-aos="fade-up" data-aos-duration="1000">Contact Us</h4>
-                <li class="ftr-mn-li">
-                    <p class="ftr-mcnt">Email</p>
-                    <p class="ftr-msct">needhelp@Organia.com</p>
-                </li>
-                <li class="ftr-mn-li">
-                    <p class="ftr-mcnt">Phone</p>
-                    <p class="ftr-msct">666 888 888</p>
-                </li>
-                <li class="ftr-mn-li">
-                    <p class="ftr-mcnt">Address</p>
-                    <p class="ftr-msct">88 road, borklyn street, USA</p>
-                </li>
-            </ul>
-        </div>
-
-        <div class="col-md-6 footer-two-clm">
-            <div class="footer-center-main">
-                <img src="{{ asset('assets/Logo.svg') }}" class="img-fluid" data-aos="fade-up" data-aos-duration="1000" alt="Logo">
-                <p class="f-c-m-pera-fcm">Simply dummy text of the printing and typesetting industry. Lorem Ipsum simply dummy text of the printing</p>
-                <div class="social-main-cnct">
-                    <div class="main-icn-social">
-                        <a href="#">
-                            <img src="{{ asset('assets/Insta.png') }}" class="img-fluid" alt="Instagram">
-                        </a>
-                    </div>
-                    <div class="main-icn-social">
-                        <a href="#">
-                            <img src="{{ asset('assets/Fb.png') }}" class="img-fluid" alt="Facebook">
-                        </a>
-                    </div>
-                    <div class="main-icn-social">
-                        <a href="#">
-                            <img src="{{ asset('assets/Twiter.png') }}" class="img-fluid" alt="Twitter">
-                        </a>
-                    </div>
-                    <div class="main-icn-social">
-                        <a href="#">
-                            <img src="{{ asset('assets/Pintrest.png') }}" class="img-fluid" alt="Pinterest">
-                        </a>
-                    </div>
+        <div class="footer-modern">
+            <div class="footer-brand" data-aos="fade-up" data-aos-duration="1000">
+                <img src="{{ asset('assets/Logo.svg') }}" class="img-fluid footer-logo" alt="Logo">
+                <p class="footer-desc">Fresh, sustainable, and farm‑to‑home products that support healthy living and local farmers.</p>
+                <div class="footer-social">
+                    <a href="#" class="footer-social-link" aria-label="Instagram">
+                        <img src="{{ asset('assets/Insta.png') }}" class="img-fluid" alt="Instagram">
+                    </a>
+                    <a href="#" class="footer-social-link" aria-label="Facebook">
+                        <img src="{{ asset('assets/Fb.png') }}" class="img-fluid" alt="Facebook">
+                    </a>
+                    <a href="#" class="footer-social-link" aria-label="Twitter">
+                        <img src="{{ asset('assets/Twiter.png') }}" class="img-fluid" alt="Twitter">
+                    </a>
+                    <a href="#" class="footer-social-link" aria-label="Pinterest">
+                        <img src="{{ asset('assets/Pintrest.png') }}" class="img-fluid" alt="Pinterest">
+                    </a>
                 </div>
             </div>
-        </div>
 
-        <div class="col-md-3 footer-three-clm">
-            <ul class="ftr-mn-ul">
-                <h4 data-aos="fade-up" data-aos-duration="1000">Utility Pages</h4>
-                <li class="ftr-mn-li-right-side">
-                    <p class="ftr-msct-right-side"><a href="{{ route('style-guide') }}">Style Guide</a></p>
-                </li>
-                <li class="ftr-mn-li-right-side">
-                    <p class="ftr-msct-right-side"><a href="{{ route('error') }}">404 Not Found</a></p>
-                </li>
-                <li class="ftr-mn-li-right-side">
-                    <p class="ftr-msct-right-side"><a href="{{ route('password') }}">Password Protected</a></p>
-                </li>
-                <li class="ftr-mn-li-right-side">
-                    <p class="ftr-msct-right-side"><a href="{{ route('licenses') }}">Licences</a></p>
-                </li>
-                <li class="ftr-mn-li-right-side">
-                    <p class="ftr-msct-right-side"><a href="{{ route('changelog') }}">Changelog</a></p>
-                </li>
-            </ul>
+            <div class="footer-links" data-aos="fade-up" data-aos-duration="1000">
+                <h4>Quick Links</h4>
+                <ul class="footer-links-list">
+                    <li><a href="{{ route('home') }}">Home</a></li>
+                    <li><a href="{{ route('shop') }}">Shop</a></li>
+                    <li><a href="{{ route('about') }}">About Us</a></li>
+                    <li><a href="{{ route('services') }}">Services</a></li>
+                    <li><a href="{{ route('contact') }}">Contact</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-contact" data-aos="fade-up" data-aos-duration="1000">
+                <h4>Contact Us</h4>
+                <ul class="footer-contact-list">
+                    <li>
+                        <span class="footer-label">Email</span>
+                        <span class="footer-value">needhelp@Organia.com</span>
+                    </li>
+                    <li>
+                        <span class="footer-label">Phone</span>
+                        <span class="footer-value">666 888 888</span>
+                    </li>
+                    <li>
+                        <span class="footer-label">Address</span>
+                        <span class="footer-value">Rajkot, Gujarat, India</span>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
     </div>
 </div>
 <div class="footer-copy-right">
